885dec052615a1d9ebc5cea3d038598a2b037a07
Brian Watson <bwats2@gmail.com>
2019-11-03 17:09:48 -0500

    Gen list of NFL Teams

--- gen list of nfl teams.py
+++ gen list of nfl teams.py
@@ -1,1 +1,17 @@
-NFL_TEAM_LIST: ''
+import csv
+
+team_list = []
+# Open csv file
+with open('data 2019 CSV.csv') as csvfile:
+    # Skip first line (headers)
+    csvfile.readline()
+    # Use delimiter to parse data columns
+    readCSV = csv.reader(csvfile, delimiter=',')
+    # Iterate over rows
+    for row in readCSV:
+        # Read second column of data
+        x = row[1]
+        # Check that valid zipcode
+        team_list.append(x)
+
+print(sorted(team_list))
