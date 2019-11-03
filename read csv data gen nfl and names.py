ff5869bd7fd8e78887571680a45140356a73176a
Brian Watson <bwats2@gmail.com>
2019-11-03 17:36:18 -0500

    Create read csv data gen nfl and names.py
    read both nfl teams, and players, from csv file

---
+++ read csv data gen nfl and names.py
@@ -0,0 +1,19 @@
+import csv
+
+team_list = []    # Store NFL Team Names
+names = set()    # Store player names
+with open('data 2019 CSV.csv') as csvfile:
+    csvfile.readline()
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        x = row[1]    # Second Column
+        team_list.append(x)
+        y = row[2]    # Third Column, Containing People/Names
+        names.add(y)
+
+for item in team_list:
+    print(item, len(item.split()))
+
+names.remove('Alex il Butte')
+names.add('Alex Butensky')
+print(names)
