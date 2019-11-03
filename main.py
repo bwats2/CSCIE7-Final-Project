05bbfba523f38c64c0e2f08bc83ed09d84bac9ff
Brian Watson <bwats2@gmail.com>
2019-11-03 17:09:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+NFL_TEAMS = ['Arizona Cardinals', 'Atlanta Falcons', 'Baltimore Ravens', 'Buffalo Bills', 'Carolina Panthers', 'Chicago Bears', 'Cincinnati Bengals', 'Cleveland Browns', 'Dallas Cowboys', 'Denver Broncos', 'Detroit Lions', 'Green Bay Packers', 'Houston Texans', 'Indianapolis Colts', 'Jacksonville Jaguars', 'Kansas City Chiefs', 'Los Angeles Chargers', 'Los Angeles Rams', 'Miami Dolphins', 'Minnesota Vikings', 'New England Patriots', 'New Orleans Saints', 'New York Giants', 'New York Jets', 'Oakland Raiders', 'Philadelphia Eagles', 'Pittsburgh Steelers', 'San Francisco 49ers', 'Seattle Seahawks', 'Tampa Bay Buccaneers', 'Tennessee Titans', 'Washington Redskins']
+NFL_TEAMS_SHORT = []
+for team in NFL_TEAMS:
+    short = team.split()[-1]
+    NFL_TEAMS_SHORT.append(short)
+print(NFL_TEAMS_SHORT)
+
+# FUNCTION Declare number of players, with names
+# Determine random order
+
+# FUNCTION draft
+# Insert into spreadsheet
+
+# FUNCTION metrics/stats call
+# Web scrape
+# Lena lecture beautiful soup 11/12
