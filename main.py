1a59ecbcb7563611964035bdbd8062d3602e1555
Brian Watson <bwats2@gmail.com>
2019-11-13 14:40:00 -0500

    updated

--- main.py
+++ main.py
@@ -2,15 +2,38 @@ import random
 
 NFL_TEAMS = ['Arizona Cardinals', 'Atlanta Falcons', 'Baltimore Ravens', 'Buffalo Bills', 'Carolina Panthers', 'Chicago Bears', 'Cincinnati Bengals', 'Cleveland Browns', 'Dallas Cowboys', 'Denver Broncos', 'Detroit Lions', 'Green Bay Packers', 'Houston Texans', 'Indianapolis Colts', 'Jacksonville Jaguars', 'Kansas City Chiefs', 'Los Angeles Chargers', 'Los Angeles Rams', 'Miami Dolphins', 'Minnesota Vikings', 'New England Patriots', 'New Orleans Saints', 'New York Giants', 'New York Jets', 'Oakland Raiders', 'Philadelphia Eagles', 'Pittsburgh Steelers', 'San Francisco 49ers', 'Seattle Seahawks', 'Tampa Bay Buccaneers', 'Tennessee Titans', 'Washington Redskins']
 NFL_TEAMS_SHORT = [(team.split()[-1]) for team in NFL_TEAMS]
-PLAYERS = ['Jeremy Koo', 'Alex Butensky', 'Brian Watson', 'Jeremy Simon']
+
+PLAYERS = []
+PLAYERS_COUNT = 0
+while True:
+    inputted = input("Enter Player's Name (or 'exit'?): ")
+    if inputted.lower() == 'exit':
+        break
+    PLAYERS.append(inputted)
+PLAYERS_COUNT = len(PLAYERS)
 
 random.shuffle(PLAYERS)
 
 while NFL_TEAMS_SHORT != 0:
+    for players in PLAYERS:
+        inputted = True
+        try:
+            inputted = input(f"{player}, choose your team:")
+            assert inputted in NFL_TEAMS_SHORT
+        except:
+            print("Please enter a valid team name!")
+            continue
+        
+while NFL_TEAMS_SHORT != 0:
     for player in PLAYERS:
-        inputted = input(f"{player} choose your team:")
-        if inputted not in NFL_TEAMS_SHORT:
-            raise ValueError("Please enter a valid team")
+        inputted = True
+        while inputted not in NFL_TEAMS_SHORT:
+            inputted = input(f"{player}, choose your team:")
+            try:
+               inputted in NFL_TEAMS_SHORT 
+            except:
+                print("Please enter a valid team name!")
+                continue
         NFL_TEAMS_SHORT.remove(inputted)
 
 
